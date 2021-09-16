0bfa3ddfaf4070fd38ac5769c6aea7ce58c17a41
pmorg <pmorg@umich.edu>
2021-09-16 13:54:39 -0400

    added landmark lists rows

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Patrick Morgan on 9/16/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
