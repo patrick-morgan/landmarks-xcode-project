4355e0641db6192b56d3c613961e8e7ccfdf8c33
pmorg <pmorg@umich.edu>
2021-09-16 22:13:25 -0400

    added badge drawing

--- Landmarks/LandmarksApp.swift
+++ Landmarks/LandmarksApp.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 @main
 struct LandmarksApp: App {
+    @StateObject private var modelData = ModelData()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
